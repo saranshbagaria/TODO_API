a946f2f82a7704b410b9ca8e85f79a8e928c6946
saranshbagaria <saranshb964@gmail.com>
2023-01-01 16:59:30 +0530

    fial

---
+++ README.md
@@ -0,0 +1,4 @@
+# TODO_API
+
+This is Todo api build with expressjs and nodejs most intresting part is i used 
+typescript for this project.
--- config/default.ts
+++ config/default.ts
@@ -1,5 +1,6 @@
 export default {
     port : 3500,
     host : 'localhost',
-    dbUri: "mongodb+srv://saransh_bagaria:lKCjjgoLKbrbIU0b@cluster0.ezywf9e.mongodb.net/?retryWrites=true&w=majority"
+    //TODO: add your mongoDB url here
+    dbUri: ""
 }
