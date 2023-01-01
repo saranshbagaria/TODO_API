531a76f92044eaa49a028dfd0527c989c3d4b6d9
saranshbagaria <saranshb964@gmail.com>
2023-01-01 14:43:43 +0530

    swagger created

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,37 @@ import config from "config"
 import log from "./logger"
 import connect from "./db/connect"
 import todo_router from './routes/todo.routes';
+//swagger import
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import { getServers } from "dns";
+
+
+const swaggerOptions = {
+    definition : {
+        openapi: "3.0.0",
+        info : {
+            title : 'TODO api',
+            version : "1.0.0",
+            description : 'This is REST api  Todo api build with ExpressJs and nodejs most interesting part is i used typescript for this project.',
+            contact : {
+                name : "Saransh Bagaria",
+                url : 'https://github.com/saranshbagaria'
+            },
+            
+        },
+        servers : [
+            {
+                url : "http://localhost:3500",
+            },
+        ],
+    }, 
+    apis : ["./src/routes/*.ts"]
+};
+
+const swaggerDocs  = swaggerJsDoc(swaggerOptions);
+
+
 
 const PORT = config.get("port") as number;
 
@@ -11,7 +42,7 @@ const HOST = config.get("host") as string;
 const app = express();
 
 
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(express.json());
 
--- src/routes/todo.routes.ts
+++ src/routes/todo.routes.ts
@@ -8,21 +8,156 @@ const todoController: TodoC = new TodoController();
 
 const todo_router = Router();
 
+/**
+ * @swagger
+ * components:
+ *      schemas:
+ *          ToDo:
+ *              type: object
+ *              required:
+ *                  - title
+ *              properties:
+ *                  _id :
+ *                      type : string
+ *                      description : Auto generated Id
+ *                  title:
+ *                      type: string
+ *                      description : Title of the todo 
+ *                  description:
+ *                      type : string
+ *                      description : description of the todo app
+ *                  done: 
+ *                      type : boolean
+ *              example : 
+ *                  _id: 68844c0ba662bd08dac8afe3
+ *                  title : Title for what to do
+ *                  done : false
+ *                  description : Description for ToDo title 
+ */
+
+/** 
+ * @swagger
+ *  components:
+ *      schemas:
+ *          todo_post:
+ *              type: object
+ *              required:
+ *                  - title
+ *              properties:
+ *                  title:
+ *                      type: string
+ *                      description : Title of the todo 
+ *                  description:
+ *                      type : string
+ *                      description : description of the todo app
+ *              example : 
+ *                  title : Title for what to do
+ *                  description : Description for ToDo title         
+ */
+
+/**
+ * @swagger
+ * /api/todos:
+ *  get:
+ *      summary : Returns the list of all todos available
+ *      responses : 
+ *          200: 
+ *              description : todos list
+ *              content:
+ *              application/json:
+ *                  schema:
+ *                      type: array 
+ *                      items:
+ *                          $ref : '#/components/schemas/ToDo'
+ */
+
 todo_router.get("/todos", todoController.getTodo);
 
+/**
+ * @swagger
+ * /api/add-todo:
+ *  post:
+ *      summary : Update the data of todo list
+ *      requestBody: 
+ *          required : true
+ *          content: 
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/todo_post'
+ *      responses : 
+ *          201: 
+ *              description : todos list
+ *              content:
+ *              application/json:
+ *                  schema:
+ *                      type: array 
+ *                      items:
+ *                          $ref : '#/components/schemas/ToDo'
+ */
 todo_router.post("/add-todo", todoController.addTodo);
 
-todo_router.put("/edit-todo/:id", todoController.updateTodo);
-
-todo_router.delete("/delete-todo/:id", todoController.deleteTodo);
-
-export default todo_router;
 
 
 /**
- * /api/todos
- * /api/add-todo
- * /api/edit-todo/:id
- * /delete-todo/:id
- * 
+ * @swagger
+ *  /api/edit-todo/{id}:
+ *  put:
+ *      summary : Add new todo to database
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          required : true
+ *          schema :
+ *             type : string
+ *          description : The Todo Id
+ *      requestBody: 
+ *          required : true
+ *          content:
+ *              application/json:
+ *              schema:
+ *                  type : object
+ *                  properties:
+ *                      done:
+ *                          type: boolean
+ *                          description: send done true if you finished your work
+ *                          example: true
+ *      responses : 
+ *          200: 
+ *              description : todos list
+ *              content:
+ *              application/json:
+ *                  schema:
+ *                      type: array 
+ *                      items:
+ *                          $ref : '#/components/schemas/ToDo'
  */
+todo_router.put("/edit-todo/:id", todoController.updateTodo);
+
+
+
+/**
+ * @swagger
+ *  /api/delete-todo/{id}:
+ *  delete:
+ *      summary : Delete the todo from database
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          required : true
+ *          schema :
+ *             type : string
+ *           
+ *          description : Id of todo that you want to delete
+ *      responses : 
+ *          200: 
+ *              description : todos list
+ *              content:
+ *              application/json:
+ *                  schema:
+ *                      type: array 
+ *                      items:
+ *                          $ref : '#/components/schemas/ToDo'
+ */
+todo_router.delete("/delete-todo/:id", todoController.deleteTodo);
+
+export default todo_router;
